09953aee066f0bf01ad58285036daa70b26af8ab
Jan Klan <jan@klan.com.au>
2021-01-15 13:46:15 +1030

    Update footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ export default function Footer () {
             </div>
             <div className="mt-8 md:mt-0 md:order-1">
             <p className="text-center text-base text-gray-400">
-                &copy; 2020 Richard Evans, The Sports Writer. All rights reserved.
+                &copy; 2021 Richard Llewelyn Evans, Sportswriter. All rights reserved.
             </p>
             </div>
         </div>
